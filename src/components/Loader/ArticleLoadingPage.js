853db711455dc0b745377e75c28ac4d5f1e55960
Nikhil <nikhil@yourstory.com>
2024-09-25 16:19:06 +0530

    article page loader fixed

--- src/components/Loader/ArticleLoadingPage.js
+++ src/components/Loader/ArticleLoadingPage.js
@@ -4,20 +4,20 @@ import FilterLoader from "./FilterLoader";
 
 const ArticleLoadingPage = () => {
   return (
-    <>
-      <main className="flex flex-row pt-12 lg:pt-20 gap-x-4 md:gap-x-12 container">
-        <aside className="hidden md:block md:w-3/12">
+    <main className="container">
+      <section className="flex flex-row pt-32 min-[450px]:pt-28 lg:pt-36 gap-x-4 md:gap-x-12">
+        <aside className="invisible lg:visible w-1/12 lg:w-3/12">
           <FilterLoader />
         </aside>
-        <article className="w-full md:w-9/12">
+        <article className="w-11/12 lg:w-9/12">
           <StoryLoader />
           <StoryLoader />
           <StoryLoader />
           <StoryLoader />
           <StoryLoader />
         </article>
-      </main>
-    </>
+      </section>
+    </main>
   );
 };
 
--- src/components/Loader/StoryLoader.js
+++ src/components/Loader/StoryLoader.js
@@ -5,10 +5,10 @@ const StoryLoader = () => {
     <div className="mb-16 md:mb-20">
       <div
         role="status"
-        className="space-y-8 animate-pulse space-y-0 space-x-8 rtl:space-x-reverse flex items-center mb-6">
-        <div className="flex items-center justify-center w-full h-20 md:h-24 bg-gray-300 rounded w-32 md:w-48">
+        className="animate-pulse flex mb-6 max-[450px]:gap-x-4 gap-x-8">
+        <div className="flex items-center justify-center max-[450px]:h-[4.5rem] h-[5rem] bg-gray-300 rounded max-[450px]:w-[8.5rem] w-[6.5rem]">
           <svg
-            className="w-10 h-10 text-gray-200"
+            className="w-10 h-10 text-gray-200 m-auto"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -16,10 +16,12 @@ const StoryLoader = () => {
             <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
           </svg>
         </div>
-        <div className="w-full">
-          <div className="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
-          <div className="h-3 bg-gray-200 rounded-full mb-2.5"></div>
-          <div className="h-3 bg-gray-200 rounded-full max-w-[480px] mb-2.5"></div>
+        <div className="w-full flex flex-col justify-between pt-2">
+          <div className="h-3 bg-gray-200 rounded-full w-4/12 sm:w-1/12 mb-2 max-[450px]:mb-4"></div>
+          <div>
+            <div className="h-3 bg-gray-200 rounded-full max-w-[100%] mb-2"></div>
+            <div className="h-3 bg-gray-200 rounded-full max-w-[80%] mb-2"></div>
+          </div>
         </div>
       </div>
       <div className="h-2.5 bg-gray-200 rounded-full mb-2.5"></div>
